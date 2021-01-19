60a5e3d18789e130fc749dd06c53ad88956c6c01
BlurpyxD <60700831+Blurpyx@users.noreply.github.com>
2021-01-19 19:56:56 +0100

    Another save

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+    </head>
+
+    <body>
+        <div class="navbar">
+            <a href="/pages/Projects">Projects</a>
+            <a href="/pages/About">About</a>
+            <a href="/pages/Links">Links</a>
+        </div>
+    </body>
+
+</html>
